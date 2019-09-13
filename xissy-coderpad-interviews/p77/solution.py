2389a7ca574cd9a4d0ed5759788e59a4de122433
kde713 <de.kim@mozzet.com>
2019-09-13 13:40:49 +0900

    Solve xissy-coderpad-interview p77

--- xissy-coderpad-interviews/p77/solution.py
+++ xissy-coderpad-interviews/p77/solution.py
@@ -2,5 +2,19 @@ from typing import List
 
 
 def solution(interval_list: List[tuple]) -> List[tuple]:
-    # TODO: solve
-    return interval_list
+    interval_list.sort()
+
+    merged_list = []
+    merged_interval = list(interval_list[0])
+
+    for current_index in range(1, len(interval_list)):
+        current_interval = interval_list[current_index]
+        if current_interval[0] <= merged_interval[1]:
+            merged_interval[1] = max(merged_interval[1], current_interval[1])
+        else:
+            merged_list.append(tuple(merged_interval))
+            merged_interval = list(current_interval)
+
+    merged_list.append(tuple(merged_interval))
+
+    return merged_list
