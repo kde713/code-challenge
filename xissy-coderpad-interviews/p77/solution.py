a80c9c8f3dbbdade024bb789747264e94e9bc49e
kde713 <de.kim@mozzet.com>
2019-09-13 13:50:35 +0900

    Enhance xissy-coderpad-interview p77 with pop

--- xissy-coderpad-interviews/p77/solution.py
+++ xissy-coderpad-interviews/p77/solution.py
@@ -5,10 +5,9 @@ def solution(interval_list: List[tuple]) -> List[tuple]:
     interval_list.sort()
 
     merged_list = []
-    merged_interval = list(interval_list[0])
+    merged_interval = list(interval_list.pop(0))
 
-    for current_index in range(1, len(interval_list)):
-        current_interval = interval_list[current_index]
+    for current_interval in interval_list:
         if current_interval[0] <= merged_interval[1]:
             merged_interval[1] = max(merged_interval[1], current_interval[1])
         else:
